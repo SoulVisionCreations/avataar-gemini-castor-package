9c02bce1d242367ba15604067124c78a2b890ae0
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-06-30 14:36:20 +0530

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,60 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GeminiPackage",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GeminiPackage",
+            targets: ["GeminiPackageTarget"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+         .package(url: "https://github.com/maxxfrazer/FocusEntity.git", from: "2.3.0"),
+         .package(url: "https://github.com/onevcat/Kingfisher", from : "7.6.2")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "GeminiPackageTarget",
+            dependencies: [
+                .target(
+                    name: "GeminiFrameworkWrapper",
+                    condition: .when(platforms: [.iOS])
+                ),
+            ],
+            path: "GeminiPackage"
+        ),
+        .target(
+            name: "GeminiFrameworkWrapper",
+            dependencies: [
+                .target(
+                    name: "GeminiCastorFramework",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "FocusEntity", package: "FocusEntity",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "Kingfisher", package: "Kingfisher",
+                    condition: .when(platforms: [.iOS])
+                )
+                
+            ],
+            path: "GeminiFrameworkWrapper"
+        ),
+        .binaryTarget(
+            name: "GeminiCastorFramework",
+            path: "./Sources/GeminiCastorFramework.xcframework"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# avataar-gemini-castor
+
+## Description
+The `avataar-gemini-castor` package allows you to integrate the Gemini Renderer into your iOS app. It provides a convenient way to render USDZ assets published from Avataar apollo and interact with them in your application.
+
