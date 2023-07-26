972f13ab03358cc4bc904a3a0760cb4127df6569
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-07-26 19:29:00 +0530

    Update xcframework

---
+++ Sources/GeminiCastorFramework.xcframework/GeminiCastorFramework.framework-iphoneos.xcarchive/Products/Library/Frameworks/GeminiCastorFramework.framework/Headers/GeminiCastorFramework.h
@@ -0,0 +1,18 @@
+//
+//  GeminiCastorFramework.h
+//  GeminiCastorFramework
+//
+//  Created by avataar on 21/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeminiCastorFramework.
+FOUNDATION_EXPORT double GeminiCastorFrameworkVersionNumber;
+
+//! Project version string for GeminiCastorFramework.
+FOUNDATION_EXPORT const unsigned char GeminiCastorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeminiCastorFramework/PublicHeader.h>
+
+
