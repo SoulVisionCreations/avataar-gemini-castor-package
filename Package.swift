0af4025c656d13eb65b3e62f909f9db579e53ae0
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-07-26 19:03:06 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/maxxfrazer/FocusEntity.git", from: "2.3.0"),
          .package(url: "https://github.com/onevcat/Kingfisher", from : "7.6.2")
+         .package(url: "https://github.com/mixpanel/mixpanel-swift.git", from: "4.1.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
